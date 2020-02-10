241b5c1c51f92fc33a878e6684643c683a37b457
Nathan Martinez <nathanemtz@gmail.com>
2020-02-10 11:55:47 -0800

    Updated settings

--- main_project/settings.py
+++ main_project/settings.py
@@ -20,12 +20,12 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'randomsecretkey11A12ABb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://nathanmartinez.herokuapp.com/']
 
 
 # Application definition
