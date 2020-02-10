9d41018daf26eb4104afdd2cfa394a20c9eefb61
Nathan Martinez <nathanemtz@gmail.com>
2020-02-10 14:35:43 -0800

    Updated settings

--- main_project/settings.py
+++ main_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nathanmartinez.herokuapp.com/']
+ALLOWED_HOSTS = ['nathanmartinez.herokuapp.com']
 
 
 # Application definition
