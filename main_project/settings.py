0eb214c2fc224e958d87b0f7ad847d98fb7e1f9f
Nathan Martinez <nathanemtz@gmail.com>
2020-02-11 14:25:32 -0800

    settings, added storage.py

--- main_project/settings.py
+++ main_project/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'main_project.storage.WhiteNoiseStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
