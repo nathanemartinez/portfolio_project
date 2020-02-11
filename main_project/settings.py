2805f729717f4dfa0a65485a191363f1ee9b575b
Nathan Martinez <nathanemtz@gmail.com>
2020-02-11 12:00:02 -0800

    Changed settings

--- main_project/settings.py
+++ main_project/settings.py
@@ -23,8 +23,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# If it is a string of 'True' then it is True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['nathanmartinez.herokuapp.com']
 
