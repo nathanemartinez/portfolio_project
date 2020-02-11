db3942c8145b6915ea9b593d10a599b76535403b
Nathan Martinez <nathanemtz@gmail.com>
2020-02-11 15:00:12 -0800

    settings

--- main_project/settings.py
+++ main_project/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# delete this comment
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
