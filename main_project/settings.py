e66972385c77fed6879eb88b543c487e3a964d58
Nathan Martinez <nathanemtz@gmail.com>
2020-02-11 15:11:45 -0800

    settings

--- main_project/settings.py
+++ main_project/settings.py
@@ -119,11 +119,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# delete this comment
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
