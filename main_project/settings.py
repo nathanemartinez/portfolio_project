ee8ad59eb2cfdb9e229cdf67fd364097269b9f59
Nathan Martinez <nathanemtz@gmail.com>
2020-02-15 15:02:18 -0800

    Changed settings

--- main_project/settings.py
+++ main_project/settings.py
@@ -11,8 +11,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # If it is a string of 'True' then it is True
-DEBUG = True
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['nathanmartinez.herokuapp.com']
 
