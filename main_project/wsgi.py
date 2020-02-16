d60f3c4756a79632b434073bee423feaec73c99b
Nathan Martinez <nathanemtz@gmail.com>
2020-02-16 00:54:26 -0800

    ..

--- main_project/wsgi.py
+++ main_project/wsgi.py
@@ -12,7 +12,7 @@ from django.core.wsgi import get_wsgi_application
 
 # from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main_project.settings.dev")
 
 application = get_wsgi_application()
 
